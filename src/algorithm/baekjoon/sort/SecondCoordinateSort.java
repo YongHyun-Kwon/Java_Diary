0aa12b03652012e4e3122043fd323cce17fc7f94
Hyun <yhkwon0820@gmail.com>
2022-03-10 22:54:34 +0900

    Baekjoon : Sort

---
+++ src/algorithm/baekjoon/sort/SecondCoordinateSort.java
@@ -0,0 +1,68 @@
+package algorithm.baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 11651 : 좌표 정렬하기 2
+ * 
+ * 문제 : 2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음
+ * 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력 : 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가
+ * 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+ * 
+ * 출력 : 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+ *
+ */
+public class SecondCoordinateSort {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[n][2];
+
+		StringTokenizer st = null;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		} // end for
+
+//		Arrays.sort(arr, new Comparator<int[]>() {
+//
+//			@Override
+//			public int compare(int[] o1, int[] o2) {
+//
+//				if (o1[1] == o2[1]) {
+//					return o1[0] - o2[0];
+//				} else {
+//					return o1[1] - o2[1];
+//				}
+//
+//			}
+//
+//		});
+
+		Arrays.sort(arr, (n1, n2) -> {
+			if (n1[1] == n2[1]) {
+				return n1[0] - n2[0];
+			} else {
+				return n1[1] - n2[1];
+			} // end else
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append("\n");
+		} // end for
+		System.out.println(sb);
+
+	}
+
+}
